22d6e2426ed2ae0b4e804176967d065c45e8ba9f
Markus Querman <65109364+MQrmn@users.noreply.github.com>
2023-03-27 11:44:33 +0500

    Styles configured

--- WebUi/Models/Request/RequestData.cs
+++ WebUi/Models/Request/RequestData.cs
@@ -4,21 +4,27 @@ namespace WebUi
 {
     public class RequestData
     {
+        [Required(ErrorMessage = "Поле обязательно для заполнени")]
         [RegularExpression("([0-9]+)", ErrorMessage = "Значение должно быть положительным числом")]
         public decimal ColdWaterMeterValues { get; set; }
 
+        [Required(ErrorMessage = "Поле обязательно для заполнени")]
         [RegularExpression("([0-9]+)", ErrorMessage = "Значение должно быть положительным числом")]
         public decimal HotWaterMeterValue { get; set; }
 
+        [Required(ErrorMessage = "Поле обязательно для заполнени")]
         [RegularExpression("([0-9]+)", ErrorMessage = "Значение должно быть положительным числом")]
         public decimal ElectroEnergyCommonMeterValue { get; set; }
 
+        [Required(ErrorMessage = "Поле обязательно для заполнени")]
         [RegularExpression("([0-9]+)", ErrorMessage = "Значение должно быть положительным числом")]
         public decimal ElectroEnergyDayMeterValue { get; set; }
 
+        [Required(ErrorMessage = "Поле обязательно для заполнени")]
         [RegularExpression("([0-9]+)", ErrorMessage = "Значение должно быть положительным числом")]
         public decimal ElectroEnergyNightMeterValue { get; set; }
 
+        [Required(ErrorMessage = "Поле обязательно для заполнени")]
         [RegularExpression("([0-9]+)", ErrorMessage = "Значение должно быть положительным числом")]
         [Range(1, int.MaxValue, ErrorMessage = "Значение должно быть больше нуля")]
         public int ResidentsCount { get; set; } = 1;
