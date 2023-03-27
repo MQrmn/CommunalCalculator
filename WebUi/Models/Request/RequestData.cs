baf7114f2fb42d97ec446d1cf17fb4f3fed59fc9
Markus Querman <65109364+MQrmn@users.noreply.github.com>
2023-03-27 10:20:33 +0500

    Data validation in form added

--- WebUi/Models/Request/RequestData.cs
+++ WebUi/Models/Request/RequestData.cs
@@ -1,13 +1,26 @@
-﻿namespace WebUi
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebUi
 {
     public class RequestData
     {
+        [RegularExpression("([0-9]+)", ErrorMessage = "Значение должно быть положительным числом")]
         public decimal ColdWaterMeterValues { get; set; }
-        public decimal HotWaterMeterValue { get; set; }
-        public decimal ElectroEnergyCommonMeterValue { get; set; }
-        public decimal ElectroEnergyDayMeterValue { get; set; }
-        public decimal ElectroEnergyNightMeterValue { get; set; }
-        public int ResidentsCount { get; set; } = 1;
 
+        [RegularExpression("([0-9]+)", ErrorMessage = "Значение должно быть положительным числом")]
+        public decimal HotWaterMeterValue { get; set; }
+
+        [RegularExpression("([0-9]+)", ErrorMessage = "Значение должно быть положительным числом")]
+        public decimal ElectroEnergyCommonMeterValue { get; set; }
+
+        [RegularExpression("([0-9]+)", ErrorMessage = "Значение должно быть положительным числом")]
+        public decimal ElectroEnergyDayMeterValue { get; set; }
+
+        [RegularExpression("([0-9]+)", ErrorMessage = "Значение должно быть положительным числом")]
+        public decimal ElectroEnergyNightMeterValue { get; set; }
+
+        [RegularExpression("([0-9]+)", ErrorMessage = "Значение должно быть положительным числом")]
+        [Range(1, int.MaxValue, ErrorMessage = "Значение должно быть больше нуля")]
+        public int ResidentsCount { get; set; } = 1;
     }
 }
